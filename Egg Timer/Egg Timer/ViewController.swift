71c48c7df9045a622e0b602cf925c5ae943a32ba
Wesley Reisz <wes@wesleyreisz.com>
2017-05-24 08:49:47 -0400

    Simple Timer
    Simple app that uses a timer class... one internal to a custom
    eggtimer class and another to keep the UI update to date.

--- Egg Timer/Egg Timer/ViewController.swift
+++ Egg Timer/Egg Timer/ViewController.swift
@@ -9,17 +9,60 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var lblTimerDisplay: UILabel!
+   
+    var eggTimer = EggTimer()
+    var timer : Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func pausePressed(_ sender: Any) {
+        eggTimer.pauseTimer()
+        timer.invalidate()
+        updateUI()
+    }
 
+    @IBAction func playPressed(_ sender: Any) {
+        print("Timer Running")
+        eggTimer.runTimer()
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){
+            _ in self.updateUI()
+        }
 
+    }
+    
+    @IBAction func resetPressed(_ sender: Any) {
+        eggTimer.resetTimer()
+        timer.invalidate()
+        updateUI()
+    }
+    
+    @IBAction func decrementTenPressed(_ sender: Any) {
+        print("-10 seconds here")
+        eggTimer.adjustTimer(amountToAdjust: -10)
+        updateUI()
+    }
+    
+    @IBAction func incrementTenPressed(_ sender: Any) {
+        print("-10 seconds here")
+        eggTimer.adjustTimer(amountToAdjust: 10)
+        updateUI()
+    }
+    
+    func updateUI(){
+        lblTimerDisplay.text = String(eggTimer.currentCounter)
+    }
+    
+    
 }
 
