40da2c3183dfc29d3d41dc34590d1ab26859256c
Wesley Reisz <wes@wesleyreisz.com>
2017-06-01 08:47:39 -0400

    Created a Simple Tic Tac Toe Game
    Simple game that is what it sounds like.

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Files for the Complete IOS10 Course on Udemy
 * Is It Prime
 * Times Table
 * User Phone Number: https://www.evernote.com/l/AGW-ygY0_r9DQbAe7L5Bcu9MJHogu4_vvVw
+* What's the Weather: https://www.evernote.com/l/AGXe4ja7GKpFt4U1AcF9-p13gU5Jkbi492M
+* Tic Tac Toe: https://www.evernote.com/l/AGVG7DlSdHZA4LGMWO-Uam3r4RafKgnxIF4
--- Tic Tac Toe/Tic Tac Toe/ViewController.swift
+++ Tic Tac Toe/Tic Tac Toe/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var tile_8: UIButton!
     @IBOutlet weak var tile_9: UIButton!
     
-    var playerXTurn = true
+    var playerTurn:Player = Player.PLAYER_X
+    var gameBoard = [Player](repeating: Player.NOT_SELECTED, count: 9)
+    var moves: Int = 0
     
     /*
      Todo: 
@@ -37,9 +39,7 @@ class ViewController: UIViewController {
     @IBAction func itemSelected(_ sender: Any) {
         let tileNumber = (sender as! UIButton).tag
         print("Clicked button " + String(tileNumber))
-        let tile = getTile(byId: tileNumber)
-        flipTile(tile)
-        
+        updateGameBoard(tileNumber,playerTurn)
     }
     
     override func viewDidLoad() {
@@ -53,10 +53,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func updateGameBoard(_ tileNumber: Int, _ player: Player){
+        if gameBoard[tileNumber-1]==Player.NOT_SELECTED{
+            gameBoard[tileNumber-1]=player
+            flipTile(tileNumber)
+            print(gameBoard)
+            moves += 1
+            
+            checkGameBoardForWins()
+            status()
+        }
+    }
     
-    func flipTile(_ tile:UIButton){
+    func status(){
+        print("---------------------------")
+        print("Moves: " + String(moves))
+        print("---------------------------")
+    }
+    
+    func flipTile(_ tileNumber:Int){
         var tileImageName = ""
-        if playerXTurn{
+        let tile = getTile(byId: tileNumber)
+        if playerTurn==Player.PLAYER_X{
             tileImageName="cross"
         }else{
             tileImageName="nought"
@@ -65,8 +83,77 @@ class ViewController: UIViewController {
         switchPlayerTurn()
     }
     
+    func checkGameBoardForWins(){
+        var winningCombinations = [[Int]]()
+        
+        //across
+        winningCombinations.append([1,2,3])
+        winningCombinations.append([4,5,6])
+        winningCombinations.append([7,8,9])
+        
+        //down
+        winningCombinations.append([1,4,7])
+        winningCombinations.append([2,5,8])
+        winningCombinations.append([3,6,9])
+        
+        //diagonal
+        winningCombinations.append([1,5,9])
+        winningCombinations.append([3,5,7])
+        
+        for winningCombination in winningCombinations{
+            print("Combination: ")
+            print(" -" + gameBoard[winningCombination[0]-1].description())
+            print(" -" + gameBoard[winningCombination[1]-1].description())
+            print(" -" + gameBoard[winningCombination[2]-1].description())
+            
+            if compareCombinations(gameBoard[winningCombination[0]-1], gameBoard[winningCombination[1]-1], gameBoard[winningCombination[2]-1]){
+                runWinner(winningPlayer: gameBoard[winningCombination[1]-1])
+            }
+            
+        }
+        
+        if moves>=9{
+            runDraw()
+        }
+    }
+    
+    func compareCombinations(_ val1: Player, _ val2: Player, _ val3: Player) -> Bool{
+        if val1==Player.NOT_SELECTED || val2==Player.NOT_SELECTED || val3 == Player.NOT_SELECTED{
+            return false
+        }
+        
+        if val1==val2{
+            if val2==val3{
+                return true
+            }
+        }
+        
+        return false
+    }
+    
+    func runDraw(){
+        print("Draw")
+        enableGameBoard(false)
+        
+        let alert = UIAlertController(title: "Game Over", message: "This one was a draw", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func runWinner(winningPlayer: Player){
+        print("Winner" + winningPlayer.description())
+        enableGameBoard(false)
+        
+        let alert = UIAlertController(title: "Game Over", message: winningPlayer.description() + " Wins!!", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
     func resetBoard(){
-        playerXTurn = true
+        moves = 0
+        gameBoard = [Player](repeating: Player.NOT_SELECTED, count: 9)
+        enableGameBoard(true)
+        playerTurn=Player.PLAYER_X
         
         tile_1.setImage(UIImage(named: "blank") as UIImage?, for: .normal)
         tile_2.setImage(UIImage(named: "blank") as UIImage?, for: .normal)
@@ -101,12 +188,56 @@ class ViewController: UIViewController {
     }
 
     func switchPlayerTurn(){
-        if playerXTurn{
-            playerXTurn = false
+        if playerTurn==Player.PLAYER_X{
+            playerTurn = Player.PLAYER_Y
         }else{
-            playerXTurn = true
+            playerTurn = Player.PLAYER_X
+        }
+    }
+    
+    func enableGameBoard(_ val: Bool){
+        if val{
+            tile_1.isUserInteractionEnabled=true
+            tile_2.isUserInteractionEnabled=true
+            tile_3.isUserInteractionEnabled=true
+            
+            tile_4.isUserInteractionEnabled=true
+            tile_5.isUserInteractionEnabled=true
+            tile_6.isUserInteractionEnabled=true
+            
+            tile_7.isUserInteractionEnabled=true
+            tile_8.isUserInteractionEnabled=true
+            tile_9.isUserInteractionEnabled=true
+        }else{
+            tile_1.isUserInteractionEnabled=false
+            tile_2.isUserInteractionEnabled=false
+            tile_3.isUserInteractionEnabled=false
+            
+            tile_4.isUserInteractionEnabled=false
+            tile_5.isUserInteractionEnabled=false
+            tile_6.isUserInteractionEnabled=false
+            
+            tile_7.isUserInteractionEnabled=false
+            tile_8.isUserInteractionEnabled=false
+            tile_9.isUserInteractionEnabled=false
         }
     }
 
+}
+
+enum Player {
+    case PLAYER_X
+    case PLAYER_Y
+    case NOT_SELECTED
+    
+    func description() -> String{
+        if Player.PLAYER_X==self{
+            return "Player X"
+        }else if Player.PLAYER_Y==self{
+            return "Player O"
+        }else{
+            return "Not Played"
+        }
+    }
 }
 
