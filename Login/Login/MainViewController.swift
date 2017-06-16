8aecb560ecfe5c3ef9fdf5675a4247669da38f50
Wesley Reisz <wes@wesleyreisz.com>
2017-06-16 08:21:40 -0400

    Edit account feature (#18)
    * Added an Edit Feature
    
    Observation attaching segways to views on a page is easy, but quickly
    leads to a mess of segues. It seems a better approach to have named
    navigational elements to each view with a segway, then using logic
    and the preformSegue command to call them.

--- Login/Login/MainViewController.swift
+++ Login/Login/MainViewController.swift
@@ -18,7 +18,6 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         lblUserName.text = authUser.fullName
-        
     }
     
     override func didReceiveMemoryWarning() {
@@ -31,4 +30,17 @@ class MainViewController: UIViewController {
         authUser = UserDTO()
     }
     
+    @IBAction func editAccountClicked(_ sender: Any) {
+        print("Editting Account")
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //set the target controller to edit
+        if segue.identifier == "editAccountSegue"{
+            let controller = segue.destination as! RegisterViewController
+            controller.action = "edit"
+            
+        }
+    }
 }
--- Login/Login/UserDAO.swift
+++ Login/Login/UserDAO.swift
@@ -16,11 +16,11 @@ class UserDAO{
         self.appDelegate = appDelegate
     }
     
-    func authenticateUser(withUsername usrname:String, andWithPassword password:String) -> (Bool, UserDTO){
-        
+    func getUser(withId identifier:String) -> UserDTO{
         let context = self.appDelegate.persistentContainer.viewContext
         
         let request = NSFetchRequest<NSFetchRequestResult>(entityName: LoginConstants.USER_TABLE)
+        request.predicate = NSPredicate(format: "userName = %@", identifier)
         request.returnsObjectsAsFaults = false
         do{
             let results = try context.fetch(request)
@@ -28,38 +28,85 @@ class UserDAO{
             let usr = UserDTO()
             
             if results.count>0{
+                print("User Found")
+                for result in results as! [NSManagedObject]{
+                    guard let usrName = result.value(forKey: "userName") else {
+                        print("No Username found")
+                        return UserDTO()
+                    }
+                    
+                    guard let firstName = result.value(forKey: "firstName") else{
+                        print("No first name found")
+                        return UserDTO()
+                    }
+                    guard let lastName = result.value(forKey: "lastName") else{
+                        print("No last name found")
+                        return UserDTO()
+                    }
+                    
+                    
+                    usr.firstName = firstName as! String
+                    usr.lastName = lastName as! String
+                    usr.userName = usrName as! String
+                    
+                    usr.fullName = usr.firstName + " " + usr.lastName
+                    
+                    return usr
+                    
+                }
+            }else{
+                print("User Not Found")
+                return UserDTO()
+            }
+        }catch{
+            print("Unable to fetch data")
+            return UserDTO()
+        }
+        
+        return UserDTO()
+    }
+    
+    func authenticateUser(withUsername usrname:String, andWithPassword password:String) -> (Bool, UserDTO){
+        
+        let context = self.appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: LoginConstants.USER_TABLE)
+        request.predicate = NSPredicate(format: "userName = %@ && pass = %@", usrname, password)
+        request.returnsObjectsAsFaults = false
+        do{
+            let results = try context.fetch(request)
+            
+            let usr = UserDTO()
+            
+            if results.count>0{
+                print("User Found")
                 for result in results as! [NSManagedObject]{
                     guard let usrName = result.value(forKey: "userName") else {
                         print("No Username found")
                         return (false, UserDTO())
                     }
-                    guard let pass = result.value(forKey: "pass") else {
-                        print("No Password found")
+                    
+                    guard let firstName = result.value(forKey: "firstName") else{
+                        print("No first name found")
+                        return (false, UserDTO())
+                    }
+                    guard let lastName = result.value(forKey: "lastName") else{
+                        print("No last name found")
                         return (false, UserDTO())
                     }
                     
-                    if String(describing: usrName)==usrname && String(describing: pass)==password{
-                        guard let firstName = result.value(forKey: "firstName") else{
-                            print("No first name found")
-                            return (false, UserDTO())
-                        }
-                        guard let lastName = result.value(forKey: "lastName") else{
-                            print("No last name found")
-                            return (false, UserDTO())
-                        }
-                        
-                        
-                        usr.firstName = firstName as! String
-                        usr.lastName = lastName as! String
-                        usr.userName = usrName as! String
-                        
-                        usr.fullName = usr.firstName + " " + usr.lastName
-                        
-                        return (true, usr)
-                    }
+                    
+                    usr.firstName = firstName as! String
+                    usr.lastName = lastName as! String
+                    usr.userName = usrName as! String
+                    
+                    usr.fullName = usr.firstName + " " + usr.lastName
+                    
+                    return (true, usr)
+
                 }
             }else{
-                print("No Data Found")
+                print("User Not Found")
                 return (false, UserDTO())
             }
         }catch{
@@ -91,4 +138,37 @@ class UserDAO{
         }
     }
     
+    func updateUser(withFirstName firstName:String,
+                    withLastName lastName:String,
+                    withUserName usrName:String,
+                    withPass pass:String) -> Bool{
+        let context = self.appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: LoginConstants.USER_TABLE)
+        request.predicate = NSPredicate(format: "userName = %@", usrName)
+        request.returnsObjectsAsFaults = false
+        
+        
+        do{
+            //fetch the record to update
+            let results = try context.fetch(request)
+            if results.count>0{
+                print("User Found")
+                //update
+                for result in results as! [NSManagedObject]{
+                    result.setValue(usrName, forKey: "userName")
+                    result.setValue(firstName, forKey: "firstName")
+                    result.setValue(lastName, forKey: "lastName")
+                    result.setValue(pass, forKey: "pass")
+                }
+            }
+            //save the results
+            try context.save()
+        }catch{
+            print("Unable to add account")
+            return false
+        }
+    
+        return true
+    }
 }
